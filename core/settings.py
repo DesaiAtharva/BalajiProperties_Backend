9b509fc9f450feaead8eff76f83a4e4ccee80f82
DesaiAtharva <desaiatharva20@gmail.com>
2026-05-14 11:51:28 +0530

    Security: Add Vercel frontend to CORS whitelist

--- core/settings.py
+++ core/settings.py
@@ -116,6 +116,7 @@ CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
+    "https://balaji-properties-o874-git-main-atharvas-projects-4caa8f71.vercel.app",
 ]
 
 # REST Framework Settings
